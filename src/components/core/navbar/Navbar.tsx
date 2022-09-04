514e80e10058c46b94a117dada283a973b7af26d
Mau MD <j.mauricio.munoz1@gmail.com>
2022-09-04 09:07:29 -0700

    Adding navbar from Mantine ui

---
+++ src/components/core/navbar/Navbar.tsx
@@ -0,0 +1,100 @@
+import { Navbar, Group, Code, ScrollArea, createStyles } from "@mantine/core";
+import {
+  IconNotes,
+  IconCalendarStats,
+  IconGauge,
+  IconPresentationAnalytics,
+  IconFileAnalytics,
+  IconAdjustments,
+  IconLock,
+  IconTrophy,
+} from "@tabler/icons";
+import { UserButton } from "./UserButton";
+import { LinksGroup } from "./LinksGroup";
+import Logo from "../Logo";
+
+const mockdata = [
+  { label: "Dashboard", icon: IconGauge },
+  {
+    label: "Problems of the Week",
+    icon: IconNotes,
+  },
+  {
+    label: "Upcoming Events",
+    icon: IconCalendarStats,
+  },
+  { label: "Project Gallery", icon: IconPresentationAnalytics },
+  { label: "Leetcode Contest", icon: IconTrophy },
+];
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  header: {
+    padding: theme.spacing.md,
+    paddingTop: 0,
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    borderBottom: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  links: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+  },
+
+  linksInner: {
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+  },
+
+  footer: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}));
+
+export function NavbarNested() {
+  const { classes } = useStyles();
+  const links = mockdata.map((item) => (
+    <LinksGroup {...item} key={item.label} />
+  ));
+
+  return (
+    <Navbar
+      height={"100vh"}
+      width={{ sm: 300 }}
+      p="md"
+      className={classes.navbar}
+    >
+      <Navbar.Section className={classes.header}>
+        <Group position="apart">
+          <Logo />
+          <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
+        </Group>
+      </Navbar.Section>
+
+      <Navbar.Section grow className={classes.links} component={ScrollArea}>
+        <div className={classes.linksInner}>{links}</div>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.footer}>
+        <UserButton
+          image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+          name="Ann Nullpointer"
+          email="anullpointer@yahoo.com"
+        />
+      </Navbar.Section>
+    </Navbar>
+  );
+}

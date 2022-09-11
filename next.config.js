a438aee493f6318e6a428521f089e239c1e4a254
Mau MD <j.mauricio.munoz1@gmail.com>
2022-09-10 17:33:04 -0700

    fixed next config to enable full calendar to work

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// lo agregue para usar el full calendar --Daniel B.
+/** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+]);
+
+module.exports = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+});

d1cbdf60a32a5ac9216796ddb834526e0cbe0af2
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-10-05 19:44:24 -0700

    Fixed wierd card width

--- src/components/project/Cards.tsx
+++ src/components/project/Cards.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ProjectView = () =>  {
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder style={{ width: "20%", maxWidth: "1000px" }}>
+    <Card shadow="sm" p="lg" radius="md" withBorder style={{ width: "100%" }}>
       <Card.Section component="a" href="https://mantine.dev/">
         <Image
           src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
--- src/components/project/ProjectRoot.tsx
+++ src/components/project/ProjectRoot.tsx
@@ -1,29 +1,44 @@
-import { Card, Title,Text, Space, Grid, Button, Affix } from "@mantine/core";
+import { Card, Title,Text, Space, Grid, Button, Affix, Group, Modal } from "@mantine/core";
+import { useState } from 'react';
 import ProjectView from "./Cards";
 import React from "react";
 import Cards from "./Cards";
 
 const ProjectRoot = () => {
+  const [opened, setOpened] = useState(false);
   return (
-    <div>
+    <>
       <Title>Project Gallery</Title>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Button> Upload new project </Button>
-      </Affix>
       <Space w="lg" h="lg" />
       <Grid>
-        <Space w="md" h="md" />
-        <ProjectView />
-        <Space w="md" h="md" />
-        <ProjectView />
-        <Space w="md" h="md" />
-        <ProjectView />
-        <Space w="md" h="md" />
-        <ProjectView />
-        <Space w="md" h="md" />
-        <ProjectView />
+        <Grid.Col span={4}>
+          <Space w="md" />
+          <ProjectView />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Space w="md" />
+          <ProjectView />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Space w="md" />
+          <ProjectView />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Space w="md" />
+          <ProjectView />
+        </Grid.Col>
       </Grid>
-    </div>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Introduce yourself!"
+      >
+        
+      </Modal>
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Button onClick={() => setOpened(true)}> Upload new project </Button>
+      </Affix>
+    </>
 
   )
 };

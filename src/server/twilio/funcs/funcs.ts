e849ae13c4b2ccbfcc3355cc3c55e64c7992d219
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-10-15 21:47:35 -0700

    added a mail function

--- src/server/twilio/funcs/funcs.ts
+++ src/server/twilio/funcs/funcs.ts
@@ -1,19 +1,18 @@
 import sgMail from '../sendgrid'
 
-
-const msg = {
-    to: 'internhub99@gmail.com',
-    from: 'internhub99@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
+export const sendEmail = (to_email: string) => {
   sgMail
-    .send(msg)
+    .send({
+      to: to_email,
+      from: 'internhub99@gmail.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    })
     .then(() => {
       console.log('Email sent')
     })
     .catch((error: any) => {
       console.error(error)
     })
-  
+}

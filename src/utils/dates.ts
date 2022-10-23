aaa02cc182a260385072b218f9c9f7ca1401c001
Mauricio Munoz <j.mauricio.munoz1@gmail.com>
2022-10-23 12:16:36 -0700

    Week change now starts on friday

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,7 +1,8 @@
-import { getWeek } from "date-fns";
+import { getWeek, addDays } from "date-fns";
 
 const STARTING_WEEK = 40;
 
 export const getRelativeWeek = () => {
-  return getWeek(new Date()) - STARTING_WEEK + 1;
+  // We subtract 5 days so the week 'starts' on Friday
+  return getWeek(addDays(new Date(), -5)) - STARTING_WEEK + 1;
 };
